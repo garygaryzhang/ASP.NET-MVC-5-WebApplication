bbb165c8ab6b8ed7f17719e73ea2e03131cd034d
Cheng Zhang <cz284@njit.edu>
2017-07-13 15:56:00 -0400

    Add project files.

---
+++ MVCapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCapp.MvcApplication" Language="C#" %>
---
+++ MVCapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCapp.Startup))]
+namespace MVCapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
