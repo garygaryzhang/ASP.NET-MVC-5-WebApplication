d3356bb712a16f10cd9e9499304fd5ea535b7c63
Cheng Zhang <cz284@njit.edu>
2017-07-13 17:14:49 -0400

    first commit

--- MVCapp/App_Start/BundleConfig.cs
+++ MVCapp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MVCapp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
